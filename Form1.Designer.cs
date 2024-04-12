9b19d9b12ae1bb8438643c8baca75584b841b42e
admin <admin@DESKTOP-BOTTGEF>
2024-04-12 12:10:00 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace WorldGuessingGameWindowsApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtbox2 = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtbox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.panel1.Controls.Add(this.txtbox2);
+            this.panel1.Location = new System.Drawing.Point(38, 54);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(445, 107);
+            this.panel1.TabIndex = 0;
+            // 
+            // txtbox2
+            // 
+            this.txtbox2.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.txtbox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtbox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbox2.ForeColor = System.Drawing.Color.White;
+            this.txtbox2.Location = new System.Drawing.Point(139, 29);
+            this.txtbox2.Name = "txtbox2";
+            this.txtbox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtbox2.Size = new System.Drawing.Size(170, 40);
+            this.txtbox2.TabIndex = 0;
+            this.txtbox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.flowLayoutPanel1.Controls.Add(this.label2);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(524, 40);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(236, 36);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.MaximumSize = new System.Drawing.Size(200, 30);
+            this.label2.MinimumSize = new System.Drawing.Size(200, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(200, 30);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "      Wrong Guess";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtbox1
+            // 
+            this.txtbox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbox1.Location = new System.Drawing.Point(92, 176);
+            this.txtbox1.Name = "txtbox1";
+            this.txtbox1.Size = new System.Drawing.Size(338, 29);
+            this.txtbox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(181, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 51);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Guess";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(524, 83);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(236, 173);
+            this.listBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtbox1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.TextBox txtbox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox txtbox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WorldGuessingGameWindowsApplication
+{
+    public partial class Form1 : Form
+    {
+        private string guessWord = "Computer";
+
+        public Form1()
+        {
+            InitializeComponent();
+            HideWord();
+        }
+
+        private void HideWord()
+        {
+            string Hideword = "C??????r";
+            txtbox2.Text = Hideword;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string guess = txtbox1.Text;
+
+            if (guess.Equals(guessWord))
+            {
+                txtbox2.Text = guessWord;
+                MessageBox.Show("Amazing! You've guessed the word" + "\n" + "The word is " + guessWord);
+            }
+            else
+            {
+                listBox1.Items.Add(guess);
+                MessageBox.Show("Keep guessing, You can do it");
+            }
+        }
+    }
+}
